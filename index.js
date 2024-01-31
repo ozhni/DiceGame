f7f992f55c524cc149f09592ff5bf4c687ce47ba
ozhni <mahsaqholami@gmail.com>
2024-01-31 18:36:16 +0330

    start look

--- index.js
+++ index.js
@@ -12,3 +12,25 @@ const diceEl = document.querySelector(".dice");
 const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
+
+let scores, currentscore, activePlayer, playing;
+
+//starting conditions
+const init = function () {
+  scores = [0, 0];
+  currentscore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add("hidden");
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
+};
+init();
